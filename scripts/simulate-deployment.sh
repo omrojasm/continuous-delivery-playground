2b81387f6113a1fc87ad454ac873b11616574b42
oscarrojas1977 <oscar.rojasmaillo@thoughtworks.com>
2023-07-26 17:28:59 +0200

    05 - Deploy application

--- scripts/simulate-deployment.sh
+++ scripts/simulate-deployment.sh
@@ -7,7 +7,7 @@ target_environmenbt=${1:-development}
 echo "Simulating a deployment to environemnt: [${target_environmenbt}]"
 echo "Deployment started"
 echo "Deploying following files:"
-ls -R ../artifact_to_deploy
+ls -R ../modern-web-app/.next
 echo "Deployment in progress..."
 sleep 10
 echo "Deployment to environment [${target_environmenbt}] completed successfully"
